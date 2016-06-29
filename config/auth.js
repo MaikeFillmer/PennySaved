d4674f5593ba00088ad62e302e0ee730533b363f
Maike <Maike.Fillmer@gmail.com>
2016-06-28 22:22:28 -0400

    initial

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID' : '1743891522524052',
+		'clientSecret' : '11a1727e3500d9856b302ea55dc43104',
+		'callbackURL' : 'http://localhost:8080/auth/facebook/callback'
+	}
+}
---
+++ config/connection.js
@@ -0,0 +1,45 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+//
+// Dependencies
+// var Sequelize = require("sequelize");
+
+// Lists out connection options
+var source = {
+
+    localhost: {
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: "mf4711MF",
+        database: "my_schema"
+    },
+//after we deploy to heroku and add the jawsDB then we can add the missing data here:
+    jawsDB: {
+        port: 3306,
+        host     : 'g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user     : 'rpxjl8n8jkvtccs4',  
+        password : 'ctz16b9cbuxplia0',
+        database : 'r24jx5oemvxfcfaa'
+    }
+}
+
+// Selects a connection (can be changed quickly as needed)
+var selectedSource = source.localhost;
+
+// Creates mySQL connection using Sequelize
+// var sequelize = new Sequelize(selectedSource.database, selectedSource.user, selectedSource.password, {
+//   host: selectedSource.host,
+//   dialect: 'mysql',
+
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   },
+
+// });
+
+// // Exports the connection for other files to use
+// module.exports = sequelize;
