b22cb5324ff85d739876031f656b0c91aab2d16e
Maike <Maike.Fillmer@gmail.com>
2016-07-02 00:31:58 -0400

    update

--- db/schema.sql
+++ db/schema.sql
@@ -13,3 +13,15 @@ CREATE TABLE dbconfig
     UNIQUE INDEX `id_UNIQUE` (`id` ASC), 
     UNIQUE INDEX `username_UNIQUE` (`username` ASC) 
  ); 
+
+
+create table foodinfo
+(
+	id int not null auto_increment,
+    mealtype varchar(255) not null,
+    date timestamp default now() on update now(),
+    restaurant varchar(255) not null,
+    cost decimal(5,2),
+    Fid varchar(255) not null,
+    primary key (id)
+);
